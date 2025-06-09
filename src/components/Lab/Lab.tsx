64a52caa2d8266846442b88cce594d2b156c1e7e
Jeremy Kim <jeremy.vir.kim@gmail.com>
2025-06-08 18:44:27 -0700

    fix images

--- src/components/Lab/Lab.tsx
+++ src/components/Lab/Lab.tsx
@@ -9,37 +9,37 @@ export default function Lab() {
         {
             link: "https://portal-scene-flame.vercel.app/",
             description: "A portal scene made in blender and put on the web using ThreeJS. The goal was to start working with 3D modeling software to create a complex and interesting render and see how it would look on a website.",
-            image: "https://strapi-production-e78b.up.railway.app/uploads/Portal_scene_a3a6b6c9dc.png"
+            image: "https://strapi-production-e78b.up.railway.app/uploads/Portal_59d4a48b66.png"
         },
         {
             link: "https://fun-portfolio-with-r3f.vercel.app/",
             description: "A fun portfolio using React Three Fiber. I wanted to play around with this library and put the current website inside a 3D scene.",
-            image: "https://strapi-production-e78b.up.railway.app/uploads/Capture_d_ecran_2024_05_26_a_20_26_02_ff05061527.png"
+            image: "https://strapi-production-e78b.up.railway.app/uploads/Fun_Portfolio_ca862448b1.png"
         },
         {
             link: "https://18-galaxy-generator-ivory.vercel.app/",
             description: "A galaxy generator using ThreeJS. The goal was to play around particles, debugging UI and maths to create galaxies.",
-            image: "https://strapi-production-e78b.up.railway.app/uploads/Galaxy_Generator_29ee3077b4.png"
+            image: "https://strapi-production-e78b.up.railway.app/uploads/Galaxy_Generator_e4c5625158.png"
         },
         {
             link: "https://19-scroll-based-animation-three.vercel.app/",
             description: "This time I'm mixing ThreeJS with HTML content for the first time. Here I'm experimenting with scroll based animations.",
-            image: "https://strapi-production-e78b.up.railway.app/uploads/Scroll_Based_Animation_cb901c280a.png"            
+            image: "https://strapi-production-e78b.up.railway.app/uploads/Scroll_Based_Animation_e7ab133f6c.png"            
         },
         {
             link: "https://20-physics-one.vercel.app/",
             description: "Combining ThreeJS with CannonJS to create a physics simulation. Don't burn your CPU!",
-            image: "https://strapi-production-e78b.up.railway.app/uploads/Physics_8bcd3c0c7f.png"
+            image: "https://strapi-production-e78b.up.railway.app/uploads/Physics_3f97fd15ed.png"
         },
         {
             link: "http://jeremykim.fr/Tilevania/",
             description: "I was able to learn tiles, tilemaps and how we can easily build levels with them. Physics and collision also played a big part of this course. This time, the game is a simple platformer where you need to reach the exit.",
-            image: "https://strapi-production-e78b.up.railway.app/uploads/Tilevania_8dde585_7211fdc79c.png"
+            image: "https://strapi-production-e78b.up.railway.app/uploads/Tilevania_8dde585_c020dd8f9a.png"
         },
         {
             link: "http://jeremykim.fr/Glitch-Garden/",
             description: "This game is based on the famous Plants vs Zombies game with a tower defense system. This was a great opportunity for me to understand the animations, sprites and their relationships with the other components.",
-            image: "https://strapi-production-e78b.up.railway.app/uploads/Glitch_Garden_a3f82b9_1bd9ce2b91.png"
+            image: "https://strapi-production-e78b.up.railway.app/uploads/Glitch_Garden_a3f82b9_9e0e92a705.png"
         }
     ];
 
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -4,7 +4,7 @@ export const projects = [
       description: `Mytraffic is a company that works on mobility, they gather data to process and display on a web app. As such, we worked a lot with different visualizations like heatmaps, isochrone, and cannibalisation.
       We worked closely with our data scientists and data analysts to make sure the data displayed for our customers made sense at a big scale.
       `,
-        image: 'https://strapi-production-e78b.up.railway.app/uploads/0ed25923_eae7_4241_bc8f_8d898c72af2a_Heatmap_features_a82d20622b.png',
+        image: 'https://strapi-production-e78b.up.railway.app/uploads/0ed25923_eae7_4241_bc8f_8d898c72af2a_Heatmap_features_4b4e39083e.png',
         tags: ['Django', 'Postgres', 'Docker', 'AWS', 'Mapbox', 'D3.js'],
       visit: 'https://www.mytraffic.io/fr/',
       id: 0,
@@ -13,7 +13,7 @@ export const projects = [
       title: 'Product showcase',
       description:`This website was coded from scratch using React. It's a showcase of a product, in this case, chatGPT. 
       It's a simple website that I coded to practice my React and UI/UX skills. I also used styled-components to style the website`,
-      image: 'https://strapi-production-e78b.up.railway.app/uploads/Chat_GPT_3e73966f86.png',
+      image: 'https://strapi-production-e78b.up.railway.app/uploads/Chat_GPT_720c1a0bed.png',
       tags: ['React', 'JavaScript', 'styled-components'],
       visit: 'https://gpt-showcase.vercel.app/',
       id: 1,
@@ -22,7 +22,7 @@ export const projects = [
       title: 'Obangsaek',
       description: `A blog website using react, next.js and tailwind. I also used a headless CMS Hygraph to update the content of the website easily.
       This also gave me a chance to practice my GraphQL skills.`,
-      image: 'https://strapi-production-e78b.up.railway.app/uploads/Capture_d_ecran_2024_04_10_a_14_52_36_34ccd9fa06.png',
+      image: 'https://strapi-production-e78b.up.railway.app/uploads/Capture_d_ecran_2024_04_10_a_14_52_36_2964864f21.png',
       tags: ['React', 'Next', 'Tailwind', 'Hygraph', 'GraphQL'],
       visit: 'https://www.obangsaek.fr/',
       id: 2,
@@ -31,7 +31,7 @@ export const projects = [
       title: 'Photography portfolio',
       description: `A portfolio for my photography work. I used Strapi as a headless CMS to manage my photos. 
       I also used React and tailwind to create a modern UI.`,
-        image: 'https://strapi-production-e78b.up.railway.app/uploads/Photo_Portfolio_ca51857648.png',
+        image: 'https://strapi-production-e78b.up.railway.app/uploads/Photo_Portfolio_de69fa716b.png',
         tags: ['React', 'Strapi', 'Tailwind'],
       visit: 'https://photo-portfolio-puce.vercel.app/',
       id: 3,
@@ -39,7 +39,7 @@ export const projects = [
     {
       title: 'Jammmming',
       description: "This web application allow you to search for musics, add them to a playlist and add this playlist to your Spotify account. This web app was also made during a codecademy formation. For this project, I used react and the Spotify API.",
-      image: 'https://strapi-production-e78b.up.railway.app/uploads/projet_Jamming_e0e1cd8_a47acde09e.png',
+      image: 'https://strapi-production-e78b.up.railway.app/uploads/projet_Jamming_e0e1cd8_0e418a6339.png',
       tags: ['React', 'Spotify API'],
       visit: 'http://jamming.jeremykim.fr/',
       id: 4,
@@ -47,7 +47,7 @@ export const projects = [
     {
       title: 'Ravenous',
       description: "This is a yelp like web application that search and list restaurants depending on the location of your choice. This web app was made during a codecademy formation. For this project, I used react and the yelp API.",
-      image: 'https://strapi-production-e78b.up.railway.app/uploads/projet_Ravenous_a78f360_cdb053dfee.png',
+      image: 'https://strapi-production-e78b.up.railway.app/uploads/projet_Ravenous_a78f360_66f0f292e7.png',
       tags: ['React', 'Yelp API'],
       visit: 'http://ravenous.jeremykim.fr/',
       id: 5,
